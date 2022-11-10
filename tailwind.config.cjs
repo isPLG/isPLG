f08ca9b7e3ee2f124ecf3d3abf3b1adafba9f9ea
Vivek Sinha <viveksnh@github.com>
2022-11-10 11:26:42 -0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,7 @@ const config = require('./tailwind.theme.config')
 /**
  * Find the applicable theme color palette, or use the default one
  */
-const themeConfig = 'ambersky'
-/**process.env.THEME_KEY && config[process.env.THEME_KEY] ? config[process.env.THEME_KEY] : config.default**/
+const themeConfig = process.env.THEME_KEY && config[process.env.THEME_KEY] ? config[process.env.THEME_KEY] : config.ambersky
 const { colors } = themeConfig
 module.exports = {
     darkMode: 'class',
