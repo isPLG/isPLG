2f9fc93f84ff13a6e8b9b0efb79e907888f243b5
Vivek Sinha <viveksnh@github.com>
2022-11-11 12:53:05 -0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ export const NAV_ITEMS: NavItems = {
 export const SITE = {
     // Your site's detail?
     name: '>is{PLG}',
-    title: '>is{PLG}',
+    title: 'isPLG',
     description: 'Practical Talks On Product Led Growth & Best Practices.',
     url: 'https://isPLG.com',
     githubUrl: 'https://github.com/isPLG/isPLG',
